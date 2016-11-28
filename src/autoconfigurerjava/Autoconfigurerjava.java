779113bff790b2bf30e3a8a071ae1ff096420402
Miquéias Fernandes <Miquias Fernandes@DESKTOP-Q98311P>
2016-11-28 19:03:27 -0200

    removido imports desnecessarios

--- src/autoconfigurerjava/Autoconfigurerjava.java
+++ src/autoconfigurerjava/Autoconfigurerjava.java
@@ -12,8 +12,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import rconnector.ConnectionType;
 import rconnector.RException;
 
